a052545255897331f27c3d89eba9578674c3ede2
honeyarya115-web <honeyarya8115@gmail.com>
2026-04-19 00:30:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-// CART SYSTEM
+// ADD TO CART
 function addToCart(name, price) {
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
   cart.push({name, price});
   localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Added to Cart!");
+  alert("Added!");
 }
 
 // SHOW CART
 if (document.getElementById("cart")) {
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
   let total = 0;
-
   let cartList = document.getElementById("cart");
 
-  cart.forEach(item => {
+  cart.forEach((item, index) => {
     let li = document.createElement("li");
-    li.textContent = item.name + " - ₹" + item.price;
+
+    li.innerHTML = `
+      ${item.name} - ₹${item.price}
+      <button onclick="removeItem(${index})">Remove</button>
+    `;
+
     cartList.appendChild(li);
     total += item.price;
   });
@@ -23,35 +27,41 @@ if (document.getElementById("cart")) {
   document.getElementById("total").innerText = "Total: ₹" + total;
 }
 
-// FAKE PAYMENT
+// REMOVE ITEM
+function removeItem(index) {
+  let cart = JSON.parse(localStorage.getItem("cart"));
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  location.reload();
+}
+
+// CHECKOUT
 function checkout() {
-  alert("Payment Successful ✅ (Demo Only)");
+  alert("Payment Successful ✅");
   localStorage.removeItem("cart");
   window.location.href = "index.html";
 }
 
 // SIGNUP
 function signup() {
-  let user = document.getElementById("username").value;
-  let pass = document.getElementById("password").value;
+  let user = username.value;
+  let pass = password.value;
 
   localStorage.setItem("user", user);
   localStorage.setItem("pass", pass);
 
-  document.getElementById("message").innerText = "Signup Successful!";
+  message.innerText = "Signup Done!";
 }
 
 // LOGIN
 function login() {
-  let user = document.getElementById("username").value;
-  let pass = document.getElementById("password").value;
+  let user = username.value;
+  let pass = password.value;
 
-  let savedUser = localStorage.getItem("user");
-  let savedPass = localStorage.getItem("pass");
-
-  if (user === savedUser && pass === savedPass) {
-    document.getElementById("message").innerText = "Login Successful!";
+  if (user === localStorage.getItem("user") &&
+      pass === localStorage.getItem("pass")) {
+    message.innerText = "Login Success!";
   } else {
-    document.getElementById("message").innerText = "Wrong Credentials!";
+    message.innerText = "Wrong!";
   }
 }
