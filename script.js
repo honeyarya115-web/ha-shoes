84d3f2a851ae79686d81278b1e1f80031d85caea
honeyarya115-web <honeyarya8115@gmail.com>
2026-04-18 23:41:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-let cart = [];
-
-function addToCart(product) {
-  cart.push(product);
-  displayCart();
+// CART SYSTEM
+function addToCart(name, price) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push({name, price});
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Added to Cart!");
 }
 
-function displayCart() {
+// SHOW CART
+if (document.getElementById("cart")) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let total = 0;
+
   let cartList = document.getElementById("cart");
-  cartList.innerHTML = "";
 
   cart.forEach(item => {
     let li = document.createElement("li");
-    li.textContent = item;
+    li.textContent = item.name + " - ₹" + item.price;
     cartList.appendChild(li);
+    total += item.price;
   });
+
+  document.getElementById("total").innerText = "Total: ₹" + total;
+}
+
+// FAKE PAYMENT
+function checkout() {
+  alert("Payment Successful ✅ (Demo Only)");
+  localStorage.removeItem("cart");
+  window.location.href = "index.html";
+}
+
+// SIGNUP
+function signup() {
+  let user = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+
+  localStorage.setItem("user", user);
+  localStorage.setItem("pass", pass);
+
+  document.getElementById("message").innerText = "Signup Successful!";
+}
+
+// LOGIN
+function login() {
+  let user = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+
+  let savedUser = localStorage.getItem("user");
+  let savedPass = localStorage.getItem("pass");
+
+  if (user === savedUser && pass === savedPass) {
+    document.getElementById("message").innerText = "Login Successful!";
+  } else {
+    document.getElementById("message").innerText = "Wrong Credentials!";
+  }
 }
